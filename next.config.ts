eb862ac048a098115819e535fc6738cd4d595fe1
Dan Minshew <ofenixculpa@gmail.com>
2025-02-23 22:06:23 -0600

    configure pigment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
+import { withPigment } from '@pigment-css/nextjs-plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPigment(nextConfig);
