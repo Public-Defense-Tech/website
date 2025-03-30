d5bbf01e051132eb1ada5e82b275ceb945d3b177
Dan <ofenixculpa@gmail.com>
2025-03-30 18:25:06 -0500

    create useMessages hook

---
+++ src/hooks/useMessages.ts
@@ -0,0 +1,16 @@
+const messages = {
+  slideout1: {
+    defaultMessage:
+      "Ultimately, better data contributes to a more equitable and efficient justice system, benefiting society as a whole.",
+    description: "The first of the slide-out messages for the home page.",
+  },
+  slideout2: {
+    defaultMessage:
+      "This awareness can lead to increased funding and collaboration among stakeholders, creating a more comprehensive support network for defendants.",
+    description: "The second of the slide-out messages for the home page.",
+  },
+};
+
+const useMessages = () => {
+  return messages;
+};
